afcd7ab4a7a27ba49a8bba2fdbec57240f065837
John Boda Yang <byjohnyang@gmail.com>
2018-01-18 12:38:56 -0800

    reorganized folders + framework for interpretation set up

---
+++ python/interpret.py
@@ -0,0 +1,20 @@
+# Takes a Sklearn classification report and refactors it into human interpretable format
+# Example:
+# Classification Report: 
+#              precision    recall  f1-score   support
+#
+#           0       0.65      0.69      0.67       211
+#           1       0.51      0.46      0.48       146
+#
+# avg / total       0.59      0.60      0.59       357
+
+def interpretClassificationReport(report):
+    return "to be completed"
+
+# Takes a Sklearn confusion matrix and refactors it into human interpretable format
+# Example:
+# Confusion Matrix:
+# [[146  65]
+#  [ 79  67]]
+def interpretConfusionMatrix(matrix):
+    return "to be completed"
