e5c78791474c8c83caf953a6261aa0ec1b338b48
John Boda Yang <byjohnyang@gmail.com>
2018-01-21 11:30:42 -0800

    error messages pop up successfully

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, url_for
 from mlfunction.script import conversion
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'testkey'
 
 @app.route('/')
 def hello_world():
@@ -22,18 +23,41 @@ def upload_file():
 
 @app.route('/generate', methods=['POST'])
 def generate_results():
+    if 'csv' in request.files:
+        print(list(request.files))
+        file = request.files['csv']
+        f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(f)
+        print("Path: {0}, File Name: {1}".format(file.filename, f))
+    else:
+        flash("Test Message 1")
+
     print(list(request.form.values()))
-    print(type(request.form))
     split = request.form['split']
     y_col = request.form['label']
     print("Split: {0}. Y-Col: {1}".format(split, y_col))
 
-    file = request.files['csv']
-    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    file.save(f)
-    print("Path: {0}, File Name: {1}".format(file.filename, f))
+    modelError = False
+    splitError = False
+    labelError = False
+    returnIndex = False
 
-    return render_template('results.html')
+    if not all(x in ['svc', 'knn', 'decision-trees', 'random-forest', 'gradient-boosted'] for x in list(request.form.values())):
+        modelError = True
+        returnIndex = True
+
+    if not split:
+        splitError = True
+        returnIndex = True
+
+    if not y_col:
+        labelError = True
+        returnIndex = True
+
+    if returnIndex:
+        return render_template('index.html', splitError=splitError, labelError=labelError, modelError=modelError)
+    else:
+        return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
